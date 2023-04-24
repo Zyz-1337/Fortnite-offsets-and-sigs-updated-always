008366a6e5b2d772b12d23a9478c041b4947c554
Zyz-1337 <115790492+Zyz-1337@users.noreply.github.com>
2023-04-24 18:38:14 +0200

    Add files via upload

---
+++ offsets.cpp
@@ -0,0 +1,46 @@
+#define GWorld 0x1078a7d8
+#define FNamePool 0x10800400
+#define GetNameById 0x557c4e0
+#define DecryptAnsi 0x5f43649
+#define EncryptedViewPoint 0x106A7B40
+
+#define ProjectWorldLocationToScreen 0x7ebfa48
+namespace WorldOffsets
+{
+    DWORD PersitentLevel = 0x30;
+    DWORD GameInstance = 0x1b8;
+};
+namespace ActorOffsets
+{
+    DWORD RootComponent = 0x190;
+    DWORD PlayerState = 0x2a8; //Can be Wrong
+    DWORD Mesh = 0x310;
+};
+namespace PlayerControllerOffsets
+{
+    DWORD AcknowledgedPawn = 0x330;
+    DWORD PlayerCameraManager = 0x340;
+    DWORD GetControlRotation = 0x740; //vtable
+    DWORD SetControlRotation = 0x748; //vtable
+};
+namespace PlayerCameraManagerOffsets
+{
+    DWORD GetCameraCachePOV = 0x730; //vtable
+    DWORD GetLastFrameCameraCachePOV = 0x738; //vtable
+};
+namespace FortPawnOffsets
+{
+    DWORD CurrentVehicle = 0x2428;
+    DWORD CurrentWeapon = 0x8f8;
+    DWORD ReviveFromDBNOTime = 0x4480;
+};
+namespace PlayerStateOffsets
+{
+    DWORD TeamIndex = 0x10b0; //Can be Wrong
+};
+namespace MeshAndUSceneComponentOffsets
+{
+    DWORD ComponentToWorld = 0x240;
+    DWORD BoneArray = 0x5f0;
+};
+
---
+++ sigs.cpp
@@ -0,0 +1,7 @@
+GWorld: 48 89 05 ? ? ? ? 48 8B 4B 78
+GetBoneMatrix: E8 ? ? ? ? 0F 10 40 68
+ProjectWorldToScreen: E8 ? ? ? ? 4C 8D 5C 24 ? 41 88 06 49 8B 5B 20 49 8B 73 30 49 8B 7B 38
+FMemory::Free (FREEFN): E8 ? ? ? ? 89 6F 50
+GetNameByIndex: 40 53 48 83 EC 20 33 C0 48 8B DA 48 89 02 48 89 42 08 E8 ? ? ? ? 48 8B C3 48 83 C4 20 5B C3
+LineOfSightTo: 48 8B C4 55 53 56 57 41 54 41 55 41 56 48 8D 68 88
+GObjects: 48 8B 05 ? ? ? ? 48 8B 0C C8 48 8B 04 D1
